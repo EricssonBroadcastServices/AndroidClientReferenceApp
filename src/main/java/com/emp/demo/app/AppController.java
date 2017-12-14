96c9ed3cc8d038e1e2e35e6e80e9d65503b404d8
João Coelho <admin.henyah@gmail.com>
2017-12-14 10:42:07 +0100

    Refactoring

--- src/main/java/com/emp/demo/app/AppController.java
+++ src/main/java/com/emp/demo/app/AppController.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.Application;
 import android.app.SearchManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
