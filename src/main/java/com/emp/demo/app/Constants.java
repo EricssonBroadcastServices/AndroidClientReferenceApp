40c8f3cf5799d59273276fedc19426f6515916dd
João Coelho <admin.henyah@gmail.com>
2018-04-18 10:07:45 +0200

    Refactored constants in refapp.

--- src/main/java/com/emp/demo/app/Constants.java
+++ src/main/java/com/emp/demo/app/Constants.java
@@ -4,10 +4,19 @@ package com.emp.demo.app;
  * Created by Joao Coelho on 3/18/2017.
  */
 public class Constants {
-    public static String API_URL = "https://psempexposureapi.ebsd.ericsson.net";
+    public static String API_URL_DEV = "https://psempexposureapi.ebsd.ericsson.net";
+    public static String API_URL_PROD = "https://exposureapi.emp.ebsd.ericsson.net";
+    public static String API_URL = API_URL_DEV;
     public static String CUSTOMER = "DevGroup";
     public static String BUSSINESS_UNIT = "EnigmaTV";
 
+    //public static String CUSTOMER = "TMNLGroup";
+    //public static String BUSSINESS_UNIT = "Thuis";
+
+    //public static String CUSTOMER = "Filmbankmedia";
+    //public static String BUSSINESS_UNIT = "UK";
+
     public static String LANG = "en";
+
 }
 
